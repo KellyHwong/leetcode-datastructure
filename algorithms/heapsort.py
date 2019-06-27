ad86a4da54ef7f7e01919581334c1da0494e8ed3
KellyHwong <dianhuangkan@gmail.com>
2019-06-27 18:02:42 +0800

    heap sort done

--- algorithms/heapsort.py
+++ algorithms/heapsort.py
@@ -12,29 +12,58 @@ import random
 import sys
 
 
-def heapsort(l: list):
+def heapify(l: list, n: int, i):
+    # print(l)
+
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    max = i
+    if left < n and l[left] > l[max]:
+        max = left
+    if right < n and l[right] > l[max]:
+        max = right
+    if max != i:
+        # logger.info("exchange {} and {}".format(i, max))
+        _ = l[max]
+        l[max] = l[i]
+        l[i] = _
+        # heapify(l, len(l), max)
+        heapify(l, n, max)
+
+
+def build_heap(l: list):
     """
     注意Python参数传的是引用
     """
-    return
+    last_node = len(l) - 1
+    parent = (last_node - 1) // 2
+    for i in reversed(range(parent+1)):
+        heapify(l, len(l), i)
 
 
-def foo(l):
-    l[0] = 250
+def heap_sort(l: list):
+    """
+    注意Python参数传的是引用
+    """
+    # 建立最大堆
+    build_heap(l)
+    logger.info("build:{}".format(l))
+    for i in reversed(range(len(l))):
+        _ = l[i]
+        l[i] = l[0]
+        l[0] = _
+        logger.info(l)
+        heapify(l, n=i, i=0)
 
 
 def main():
-    l = [1, 2, 3, 4]
-    foo(l)
-    print(l)
-    return
-
-    # 构造最小堆
     # l = [random.randint(0, 99) for _ in range(10)]
     l = [78, 79, 23, 85, 42, 53, 79, 71, 3, 64]
+    # l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     logger.info(l)
-    # l.sort()
-
+    heap_sort(l)
+    logger.info(l)
     """
     用真·树构造一个堆
     """
@@ -54,9 +83,6 @@ def main():
     但是没有必要这样
     用数组就行了
     """
-    i = 0
-    left = 2*i+1
-    right = 2*i+2
 
 
 if __name__ == "__main__":
