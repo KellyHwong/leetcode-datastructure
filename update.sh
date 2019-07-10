3ca8294e493687b0b6be08f41c13f725efd1bca5
KellyHwong <dianhuangkan@gmail.com>
2019-07-10 16:54:45 +0800

    update Wed Jul 10 16:54:45 HKT 2019

--- update.sh
+++ update.sh
@@ -8,6 +8,7 @@ if which gitbook > /dev/null; then
     cd _book
     cp -R * ../../../hkust-leetcode-gitbook/
     cd ../../../hkust-leetcode-gitbook/
+    git checkout gh-pages
     git add -A
     git commit -am "update `date`"
     git push
